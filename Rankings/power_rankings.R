cad65fd2846dfef76afc05dd6b9aea14aac44e9f
colbym7 <cjmsportsfan3@gmail.com>
2025-09-30 20:10:26 -0600

    Simple BT Model 2024 vs Moneyline

--- Rankings/power_rankings.R
+++ Rankings/power_rankings.R
@@ -1,9 +1,7 @@
 library(tidyverse)
 library(BradleyTerry2)
 library(vroom)
-install.packages("https://cran.r-project.org/src/contrib/Archive/cfbfastR/cfbfastR_1.6.4.tar.gz", repos = NULL, type = "source")
-remotes::install_github("cfbfastR/cfbfastR")
-
+remotes::install_github("saiemgilani/cfbfastR", force = TRUE)
 library(cfbfastR)
 
 
@@ -88,7 +86,7 @@ expected_dif_df <- small2019 %>%
 bt1_football_mod <- BTm(cbind(HomeWin, AwayWin), HomeFactor, AwayFactor,
                         data = expected_dif_df)
 
-bt2_football_mod <- update(bt1_football_mod, formula = ~team + total_home_EPA)
+bt2_football_mod <- update(bt1_football_mod, formula = ~ team + total_home_EPA, id = 'team')
 
 summary(bt2_football_mod)
 abilities2 <- BTabilities(bt2_football_mod)
@@ -97,5 +95,132 @@ abilities2 <- BTabilities(bt2_football_mod)
 
 ### Compare Performance of BT Model with Moneyline 2023 ###
 # Data
-cfb_data2023 <- load_cfb_pbp(2023)
-bet_data2023 <- vroom('bet_data.csv')
+cfb_data2024 <- load_cfb_pbp(2024)
+bet2024 <- cfbd_betting_lines(year = 2024)
+game_results2024 <- cfbd_game_info(2024)
+
+# Prepare Data #
+game_results2024 <- game_results2024 %>%
+  filter(home_division == 'fbs' & away_division == 'fbs') %>%
+  select(-c(6,7,11,12,19,20,27:31))
+
+bet2024 <- bet2024 %>%
+  filter(home_classification == 'fbs' & away_classification == 'fbs')
+# Find out which betting provider has the least missing data
+bet2024 %>%
+  group_by(provider) %>%
+  summarise(
+    n_missing_home_moneyline = sum(is.na(home_moneyline)),
+    n_missing_away = sum(is.na(away_moneyline)),
+    n_total = n()
+  ) %>%
+  mutate(
+    pct_missing = n_missing_home_moneyline / n_total * 100
+  )
+
+bet2024 <- bet2024 %>%
+  filter(provider == 'DraftKings')
+
+cfb_full2024 <- game_results2024 %>%
+  left_join(
+    bet2024 %>%
+      select(game_id, everything()) %>%                 # ensure game_id stays
+      select(-setdiff(intersect(names(game_results2024), names(.)), "game_id")),  
+    by = "game_id"
+  )
+
+# Check NA's
+nas <- cfb_full2024 %>%
+  filter(is.na(home_points) | is.na(away_points) | is.na(home_moneyline))
+
+cfb_full2024 %>%
+  summarize(
+    home_favored = sum(home_moneyline < away_moneyline, na.rm = TRUE),
+    equal        = sum(home_moneyline == away_moneyline, na.rm = TRUE),
+    away_favored = sum(away_moneyline < home_moneyline, na.rm = TRUE)
+  )
+equal_strength <- cfb_full2024 %>%
+  filter(home_moneyline == away_moneyline)
+
+
+cfb_full2024 <- cfb_full2024 %>%
+  dplyr::select(-tidyselect::ends_with(".y")) %>%
+  mutate(
+    vegas_home_win = case_when(
+      home_moneyline < away_moneyline ~ 1,   # home favored
+      home_moneyline >= away_moneyline ~ 0,  # away favored
+      TRUE ~ NA_real_)
+  ) %>%
+  mutate(
+    HomeWin = ifelse(home_points>away_points, 1, 0),
+    AwayWin = ifelse(away_points>home_points, 1, 0)
+  ) %>%
+  mutate(
+    vegas_correct = case_when(
+      vegas_home_win == HomeWin ~ 1,
+      vegas_home_win != HomeWin ~ 0,
+      TRUE ~ NA_real_
+    )
+  )
+
+
+
+cfb_full2024$HomeWin <- as.factor(cfb_full2024$HomeWin)
+cfb_full2024$AwayWin <- as.factor(cfb_full2024$AwayWin)
+
+teams <- sort(unique(c(cfb_full2024$home_team, cfb_full2024$away_team)))
+cfb_full2024$HomeFactor <- factor(cfb_full2024$home_team, levels = teams)
+cfb_full2024$AwayFactor <- factor(cfb_full2024$away_team, levels = teams)
+  
+
+vegasVsBt <- function(week_num){
+  temp_currentweekdata <- cfb_full2024 %>%
+    filter(week == week_num)
+  temp_btdata <- cfb_full2024 %>% # Filter complete dataset to week previous for btmod
+    filter(week < week_num) %>%
+    filter(!is.na(HomeWin), !is.na(AwayWin), !is.na(HomeFactor), !is.na(AwayFactor))
+    
+  temp_vegas_data <- cfb_full2024 %>% # Filter dataset for getting betting percentages current week
+    filter(week == week_num)
+  temp_vegas_correct <- mean(temp_vegas_data$vegas_correct, na.rm = TRUE) # Betting % Correct
+
+  temp_btmod <- BTm(cbind(HomeWin, AwayWin), HomeFactor, AwayFactor,
+                                       data = temp_btdata) # BT on previous week
+  temp_abilities <- BTabilities(temp_btmod)
+  
+  temp_abilities_df <- as.data.frame(temp_abilities) %>%
+    tibble::rownames_to_column("Team")
+  
+  temp_currentweekdata <- temp_currentweekdata %>%
+    left_join(temp_abilities_df %>% rename(home_team = Team, home_ability = ability),
+              by = "home_team") %>%
+    left_join(temp_abilities_df %>% rename(away_team = Team, away_ability = ability),
+              by = "away_team")
+  temp_currentweekdata <- temp_currentweekdata %>%
+    mutate(
+      Model_homewin = ifelse(
+        home_ability >= away_ability, 1, 0
+      )
+    ) %>%
+    mutate(
+      model_correct = case_when(
+        Model_homewin == HomeWin ~ 1,
+        Model_homewin != HomeWin ~ 0,
+        TRUE ~ NA_real_
+      ))
+  model_percent <- mean(temp_currentweekdata$model_correct, na.rm = TRUE)
+  return(paste0('Model Proportion Correct:',round(model_percent,3),' ',  
+                'Vegas Proportion Correct:',round(temp_vegas_correct,3),' ', 
+                ' Model Difference: ',round(model_percent-temp_vegas_correct, 3)))
+}
+
+
+vegasVsBt(8)
+vegasVsBt(9)
+vegasVsBt(10)
+vegasVsBt(11)
+vegasVsBt(12)
+vegasVsBt(13)
+vegasVsBt(14)
+vegasVsBt(15)
+
